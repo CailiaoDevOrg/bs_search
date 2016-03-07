e21ba31dff07be31cf75edfe9e36045d9bc32346
niuyang <xfny_hmily@hotmail.com>
2016-03-07 17:57:20 +0800

    增加入口程序

---
+++ bs_search_war/src/main/java/com/whut/bssearch/war/Application.java
@@ -0,0 +1,15 @@
+package com.whut.bssearch.war;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by niuyang on 16/3/7.
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
